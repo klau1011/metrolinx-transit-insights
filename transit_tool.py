6378583dcaf8b6a1f41931598d1cde5a04e89d3a
Kevin Lau <kevlau82@gmail.com>
2023-08-27 15:33:15 -0400

    added requests to Google Place API for coordinates of each stop, .gitignore

--- transit_tool.py
+++ transit_tool.py
@@ -1,16 +1,45 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import os
+from dotenv import load_dotenv, dotenv_values
+import requests
+import requests_cache
 
-st.title('Transit Data Visualization Tool')
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+BASE_QUERY_URI = f'https://maps.googleapis.com/maps/api/place/textsearch/json?key={GOOGLE_API_KEY}&query='
+requests_cache.install_cache('places_api_cache', expire_after=3600*24*365)  
+
+st.set_page_config(
+    page_title="Transit Data Visualization Tool",
+    page_icon="🚆'",
+
+)
+
+st.title('Transit Data Visualization Tool 🚆')
 st.write("""
 
 ----
 
 """)
 
+@st.cache_data
+def stop_to_coordinate(df):
+    coordinate_array = []
+    df_locations = df[['Location']]
+    df_locations = df_locations.replace({'Zone20': 'Square One', 'Zone27': "University of Waterloo"})
+    df_locations = df_locations.squeeze()
+    
+    for index, location in enumerate(df_locations.values):
+        url = f'{BASE_QUERY_URI}{location}'
+        data = requests.get(url).json()
 
-st.write("Enter your exported Metrolinx transit data to recieve your own summaries")
+        coordinates = data["results"][0]["geometry"]["location"]
+        latitude_longitude = (coordinates['lat'], coordinates['lng'])
+
+
+st.write("Enter your exported Metrolinx transit data 📖")
 
 try: 
     uploaded_csv = st.file_uploader("Input your CSV file:", type="csv")
@@ -18,11 +47,12 @@ try:
     if not uploaded_csv:
         uploaded_csv = 'transit_usage.csv'
 
+    # Get relevant columns
     df = pd.read_csv(uploaded_csv)
     df = df[['Date', 'Location', 'Amount']]
     df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
 
-    # -- MOST FREQ STOPS --
+    # -- MOST FREQ STOPS ------
 
     # get the most 10 popular stops
     location_counts = df['Location'].value_counts().nlargest(10)
@@ -31,13 +61,14 @@ try:
     fig = px.bar(location_counts)
 
     # add axis titles
-    st.subheader('Most frequent stops')
+    st.subheader('Most frequent stops 📊')
     fig.update_layout(xaxis_title="Location", yaxis_title="Count", showlegend=False)
 
     # output graph to show most freq stops
     st.plotly_chart(fig)
 
-    # MONTHLY TRANSIT SPENDINGS 
+
+    # MONTHLY TRANSIT SPENDINGS -------
     df2 = df
     df2['Amount'] = df['Amount'].astype(str)
     df2['Amount'] = df2['Amount'].str.replace('-', '')
@@ -62,7 +93,8 @@ try:
     st.plotly_chart(amountFig)
 
 
-    # TAP ON/OFF COUNT PER MONTH 
+
+    # TAP ON/OFF COUNT PER MONTH  -----------
     df3 = df
 
     df3['Date'] = pd.to_datetime(df3['Date'])
@@ -76,5 +108,10 @@ try:
     # output graph to shows taps per months
     st.plotly_chart(fig2)
 
+    stop_to_coordinate(df2)
 except: 
     pass
+
+
+
+
