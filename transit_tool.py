b10aaf8970e48ff7f4baacb66a42437b2a356fe3
Kevin Lau <kevlau82@gmail.com>
2023-08-27 18:29:34 -0400

    added map to show stops

--- transit_tool.py
+++ transit_tool.py
@@ -5,11 +5,14 @@ import os
 from dotenv import load_dotenv, dotenv_values
 import requests
 import requests_cache
+import folium
+from folium.plugins import MarkerCluster
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 BASE_QUERY_URI = f'https://maps.googleapis.com/maps/api/place/textsearch/json?key={GOOGLE_API_KEY}&query='
-requests_cache.install_cache('places_api_cache', expire_after=3600*24*365)  
+requests_cache.install_cache('places_api_cache', expire_after=3600*24*365) 
+
 
 st.set_page_config(
     page_title="Transit Data Visualization Tool",
@@ -24,11 +27,11 @@ st.write("""
 
 """)
 
+# Turn transit stops into coordinates 
 @st.cache_data
 def stop_to_coordinate(df):
     coordinate_array = []
     df_locations = df[['Location']]
-    df_locations = df_locations.replace({'Zone20': 'Square One', 'Zone27': "University of Waterloo"})
     df_locations = df_locations.squeeze()
     
     for index, location in enumerate(df_locations.values):
@@ -37,6 +40,24 @@ def stop_to_coordinate(df):
 
         coordinates = data["results"][0]["geometry"]["location"]
         latitude_longitude = (coordinates['lat'], coordinates['lng'])
+
+        if coordinates['lng'] < -83 or coordinates['lat'] < 42.33:
+            continue
+      
+        coordinate_array.append(latitude_longitude)
+
+    # convert to df
+    coordinate_df = pd.DataFrame(coordinate_array, columns=['latitude', 'longitude'])
+    st.map(coordinate_df, size=20)
+
+def clean_raw_data(df):   
+    df = df[['Date', 'Location', 'Amount']]
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
+    df = df.replace({'Zone17': 'Aldershot GO',
+    'Zone20': 'Square One', 
+    'Zone27': "University of Waterloo"})
+
+    return df
 
 
 st.write("Enter your exported Metrolinx transit data 📖")
@@ -47,15 +68,14 @@ try:
     if not uploaded_csv:
         uploaded_csv = 'transit_usage.csv'
 
-    # Get relevant columns
     df = pd.read_csv(uploaded_csv)
-    df = df[['Date', 'Location', 'Amount']]
-    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y %I:%M:%S %p')
+
+    df = clean_raw_data(df)
 
     # -- MOST FREQ STOPS ------
 
     # get the most 10 popular stops
-    location_counts = df['Location'].value_counts().nlargest(10)
+    location_counts = df['Location'].value_counts().nlargest(7)
 
     # plot locations
     fig = px.bar(location_counts)
@@ -67,6 +87,7 @@ try:
     # output graph to show most freq stops
     st.plotly_chart(fig)
 
+    stop_to_coordinate(df)
 
     # MONTHLY TRANSIT SPENDINGS -------
     df2 = df
@@ -92,6 +113,9 @@ try:
     # output graph to show spendings per month
     st.plotly_chart(amountFig)
 
+    num_amount_spent = df2['Amount'].sum()
+    st.write(f'💸 Amount spent in the given year: {num_amount_spent}')
+
 
 
     # TAP ON/OFF COUNT PER MONTH  -----------
@@ -108,7 +132,6 @@ try:
     # output graph to shows taps per months
     st.plotly_chart(fig2)
 
-    stop_to_coordinate(df2)
 except: 
     pass
 
