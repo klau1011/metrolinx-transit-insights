da3363c2330f8af2ecc4d2b4cdb1a2cbb7e7d587
Kevin Lau <kevlau82@gmail.com>
2023-08-27 21:11:41 -0400

    Debugging test

--- transit_tool.py
+++ transit_tool.py
@@ -8,6 +8,9 @@ import requests_cache
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY', st.secrets["GOOGLE_API_KEY"])
+
+st.write(st.secrets["GOOGLE_API_KEY"])
+st.write(st.secrets)
 BASE_QUERY_URI = f'https://maps.googleapis.com/maps/api/place/textsearch/json?key={GOOGLE_API_KEY}&query='
 requests_cache.install_cache('places_api_cache', expire_after=3600*24*365) 
 
